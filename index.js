b9d5e259a7e655be0076c32360dd59e99c8bcb98
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-05-21 09:50:05 -0400

    Finished Lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(firstName){
+return `Hi, my name is ${firstName}.`;
+}
+
+
+
+
+function introductionWithLanguage(name,language) {
+return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
+
+function introductionWithLanguageOptional(name,language = "JavaScript"){
+return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
